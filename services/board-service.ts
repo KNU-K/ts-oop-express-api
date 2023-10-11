6fd207c0737bf5c716b78c0e8e959ab358418397
KNU-K <knukang334@gmail.com>
2023-10-12 03:02:23 +0900

    feat/db : board-service db연동
    feat/db : board-service db연동

--- services/board-service.ts
+++ services/board-service.ts
@@ -1,23 +1,41 @@
 import { BoardDto } from "../dto/board-dto";
+import Board from "../models/board";
 
-let boards: BoardDto[] = [];
 class BoardService {
-  static async createBoard(board: any): Promise<boolean> {
+  private static boards: any[] = [];
+  public static initBoard(boards) {
+    this.boards = boards;
+  }
+  static async createBoard(board: BoardDto, u_id: string): Promise<boolean> {
     try {
-      boards.push(board);
+      console.log(this.boards.length);
+      const createdData = {
+        no:
+          this.boards.length === 0
+            ? 1
+            : this.boards[this.boards.length - 1].no + 1,
+        ...board,
+        owner: u_id,
+      };
+      console.log(createdData);
+      const newBoard = new Board(createdData);
+      await newBoard.save();
+      this.boards.push(newBoard);
       return true;
     } catch (err) {
+      console.log(err);
       return false;
     }
   }
-  static async findBoardById(boardId: number) {
-    return boards.find((board) => board.boardId == boardId);
+  static async findBoardById(boardId: string) {
+    return this.boards.find((board) => board._id == boardId);
   }
   public static async findBoards(): Promise<BoardDto[]> {
-    return boards;
+    return this.boards;
   }
-  public static async updateBoard(boardId: number, updatedBoard: BoardDto) {
-    const board = boards.find((board) => boardId == board.boardId);
+  public static async updateBoard(boardId: string, updatedBoard) {
+    await Board.updateOne({ _id: boardId }, updatedBoard);
+    const board = this.boards.find((board) => boardId == board.boardId);
     if (board) {
       board.title = updatedBoard.title;
       board.content = updatedBoard.content;
@@ -27,10 +45,12 @@ class BoardService {
     }
   }
 
-  public static async deleteBoard(boardId: number) {
+  public static async deleteBoard(boardId: string) {
     try {
       if (!this.findBoardById(boardId)) return false;
-      boards = boards.filter((board) => boardId != board.boardId);
+
+      await Board.deleteOne({ _id: boardId });
+      this.boards = this.boards.filter((board) => boardId != board._id);
       return true;
     } catch (err) {
       return false;
